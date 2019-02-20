927d28f7874e69af9fdbca9a84465fd5da6bb4f7
Roberto Pires <roberto_pires97@sapo.pt>
2019-02-20 02:00:55 -0100

    Fixed Project Name

--- GloToolz/Program.cs
+++ GloToolz/Program.cs
@@ -1,22 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Glo_Toolz.UI.Forms;
+using System;
 using System.Windows.Forms;
 
 namespace Glo_Toolz
 {
-    static class Program
+    public static class Program
     {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        public static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Login());
         }
     }
 }
